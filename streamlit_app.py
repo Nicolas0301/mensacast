07b7bcf5ac90f47159ece77d632f5251f26dfb12
Nicolas0301 <nicilas0301@live.de>
2022-08-02 22:39:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 fig, ax = plt.subplots()
 ax.scatter(np.random.normal(0,100,100),np.random.normal(0,100,100))
